f180ea8a5a801180cf3ee236804fad08b459de69
mycoffeebean <190945573+mycoffeebean@users.noreply.github.com>
2023-09-14 15:08:07 +0800

    更新文章和增加了搜索功能

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -9,13 +9,17 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
+// 2023-09-13 23:39:42 导入了搜索插件
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
+
+
 export default defineUserConfig({
   base: "/",
 
   locales: {
     "/": {
       lang: "en-US",
-      title: "obsidian Docs CoffeeBean Edition ",
+      title: "Obsidian Docs by CoffeeBean ",
       description: "A docs for obsidian",
     },
     "/zh/": {
@@ -24,6 +28,58 @@ export default defineUserConfig({
       description: "obsidian文档和使用技巧",
     },
   },
+
+  // 2023-09-13 23:39:22 导入了搜索插件和key
+  plugins: [
+    docsearchPlugin({
+      apiKey: '402365c51dd6ecbfd8e01457ceafe0bf',
+      indexName: 'obsidianvip',
+      appId: '36KP1XM8PF',
+      locales: {
+        '/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+              buttonAriaLabel: '搜索文档'
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '清除查询条件',
+                resetButtonAriaLabel: '清除查询条件',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消'
+              },
+              startScreen: {
+                recentSearchesTitle: '搜索历史',
+                noRecentSearchesText: '没有搜索历史',
+                saveRecentSearchButtonTitle: '保存至搜索历史',
+                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏中移除'
+              },
+              errorScreen: {
+                titleText: '无法获取结果',
+                helpText: '你可能需要检查你的网络连接'
+              },
+              footer: {
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭',
+                searchByText: '搜索提供者'
+              },
+              noResultsScreen: {
+                noResultsText: '无法找到相关结果',
+                suggestedQueryText: '你可以尝试查询',
+                reportMissingResultsText: '你认为该查询应该有结果?',
+                reportMissingResultsLinkText: '点击反馈'
+              }
+            }
+          }
+        }
+      },
+    })
+  ],
 
   theme,
 
