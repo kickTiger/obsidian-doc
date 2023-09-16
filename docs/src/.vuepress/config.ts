f81f8ed4077cfc92fcc3e3dcef491f0d2be266e6
coffeebean <hnszyin@gmail.com>
2023-09-16 22:27:43 +0800

    Update config.ts

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -1,11 +1,3 @@
-/*
- * @Author: coffeebean 3623068+kickTiger@users.noreply.github.com
- * @Date: 2023-01-17 19:29:24
- * @LastEditors: coffeebean 3623068+kickTiger@users.noreply.github.com
- * @LastEditTime: 2023-01-25 20:16:41
- * @FilePath: \obsidian-doc\docs\src\.vuepress\config.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
@@ -41,6 +33,10 @@ export default defineUserConfig({
           translations: {
             button: {
               buttonText: "搜索文档",
+              /**
+               * @description: 
+               * @return {*}
+               */
               buttonAriaLabel: "搜索文档",
             },
             modal: {
